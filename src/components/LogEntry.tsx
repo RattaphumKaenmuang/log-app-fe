5280b17bef2336c4e4e9d3d6c065033532946390
Rattaphum Kaenmuang <66010704@KMITL.AC.TH>
2026-02-22 00:07:13 +0700

    Refactor LogTable to use LogEntry component and create LogData type definition

---
+++ src/components/LogEntry.tsx
@@ -0,0 +1,33 @@
+import { type LogData } from '../types/log'
+
+interface LogEntryProps {
+    log: LogData;
+}
+
+function LogEntry({ log }: LogEntryProps ) {
+    return (
+        <tr key={log._id}>
+            <td className="py-3 px-3">
+                {log.userId.prefix} {log.userId.firstname} {log.userId.lastname}
+            </td>
+            <td className="py-3 px-3">{log.request.endpoint}</td>
+            <td className="py-3 px-3">{log.request.method}</td>
+            <td className="py-3 px-3">{new Date(log.timestamp).toLocaleString()}</td>
+            <td className="py-3 px-3">{log.labnumber.join(", ")}</td>
+            <td className="py-3 px-3">{log.action}</td>
+            <td className="py-3 px-3">
+                <span className={`px-2 py-1 text-xs rounded-full ${
+                    log.response.statusCode === "200" 
+                        ? "bg-green-100 text-green-700"
+                        : "bg-red-100 text-red-700"
+                }`}>
+                    {log.response.statusCode}
+                </span>
+            </td>
+            <td className="py-3 px-3">{log.response.message}</td>
+            <td className="py-3 px-3">{log.response.timeMs}</td>
+        </tr>
+    )
+}
+
+export default LogEntry;
--- src/components/LogTable.tsx
+++ src/components/LogTable.tsx
@@ -1,26 +1,6 @@
-import { useState } from 'react'
-
-interface LogData {
-  _id: string;
-  userId: {
-    _id: string;
-    prefix: string;
-    firstname: string;
-    lastname: string;
-  };
-  request: {
-    method: string;
-    endpoint: string;
-  };
-  response: {
-    statusCode: string;
-    message: string;
-    timeMs: number;
-  };
-  timestamp: string;
-  labnumber: string[];
-  action: string;
-}
+// import { useState } from 'react'
+import { type LogData } from '../types/log'
+import LogEntry from './LogEntry'
 
 const mockLogs: LogData[] = [
   {
@@ -126,30 +106,9 @@ function LogTable() {
                         <th className="py-3 px-3">Time (ms)</th>
                     </tr>
                 </thead>
-
                 <tbody>
                     {mockLogs.map((log) => (
-                        <tr key={log._id}>
-                            <td className="py-3 px-3">
-                                {log.userId.prefix} {log.userId.firstname} {log.userId.lastname}
-                            </td>
-                            <td className="py-3 px-3">{log.request.endpoint}</td>
-                            <td className="py-3 px-3">{log.request.method}</td>
-                            <td className="py-3 px-3">{new Date(log.timestamp).toLocaleString()}</td>
-                            <td className="py-3 px-3">{log.labnumber.join(", ")}</td>
-                            <td className="py-3 px-3">{log.action}</td>
-                            <td className="py-3 px-3">
-                                <span className={`px-2 py-1 text-xs rounded-full ${
-                                    log.response.statusCode === "200" 
-                                        ? "bg-green-100 text-green-700"
-                                        : "bg-red-100 text-red-700"
-                                }`}>
-                                    {log.response.statusCode}
-                                </span>
-                            </td>
-                            <td className="py-3 px-3">{log.response.message}</td>
-                            <td className="py-3 px-3">{log.response.timeMs}</td>
-                        </tr>
+                        <LogEntry log={log} ></LogEntry>
                     ))}
                 </tbody>
             </table>
