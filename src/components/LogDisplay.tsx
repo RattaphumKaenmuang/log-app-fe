fd2a002c3564ee7c1c82c49293ee24700883222f
Rattaphum Kaenmuang <66010704@KMITL.AC.TH>
2026-02-22 18:44:53 +0700

    Add pagination to LogDisplay component and create PageButton and Pagination components

--- src/components/LogDisplay.tsx
+++ src/components/LogDisplay.tsx
@@ -3,11 +3,15 @@ import LogTable from "./LogTable";
 import { api } from '../services/api.service';
 import type { LogData } from "../types/log";
 import { useEffect, useState } from "react";
+import Pagination from "./Pagination";
+
 
 function LogDisplay() {
     const [loading, setLoading] = useState(false);
     const [logs, setLogs] = useState<LogData[]>([]);
     const [error, setError] = useState<string | null>(null);
+    const [totalPages, setTotalPages] = useState<number>(1);
+    const [page, setPage] = useState<number>(1);
     
     useEffect(() => {
         const fetchLogs = async () => {
@@ -17,6 +21,8 @@ function LogDisplay() {
                 
                 if (response.data) {
                     setLogs(response.data);
+                    setPage(response.pagination?.currentPage ?? 1)
+                    setTotalPages(response.pagination?.total ?? 1)
                 } else if (response.error) {
                     setError(response.error.message);
                 }
@@ -36,6 +42,7 @@ function LogDisplay() {
         <div className="flex flex-col items-center justify-center min-w-screen min-h-screen">
             <LogSearchBar></LogSearchBar>
             <LogTable logs={logs} loading={loading}></LogTable>
+            <Pagination page={page} totalPages={totalPages} span={5} onPageChange={setPage}></Pagination>
         </div>
     )
 }
